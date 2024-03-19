7ebff1dff71a3258a4aabf09b1aab1bad7b72990
siciliano-diag <siciliano@diag.uniroma1.it>
2024-03-19 16:33:39 +0100

    try fix circular import3

--- rec_utils/__init__.py
+++ rec_utils/__init__.py
@@ -1,4 +1,4 @@
+from . import model
 from . import data_generation_utils, metrics, losses
 from . import rec_torch_utils
-from . import model
 from ._version import __version__  # Import the '__version__' variable from this package
